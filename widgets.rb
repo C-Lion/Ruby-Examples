89acbd63e114f860a3394a941b5c523f8aa5bfcc
C-Lion <esstepansky@gmail.com>
2018-02-18 14:24:48 +0200

    add widget store file with features 1 & 2 complete

---
+++ widgets.rb
@@ -0,0 +1,22 @@
+#widgets.rb
+#Exercise as part of Ruby Basics on Treehouse.com
+#The Widget Store has 4 features: 
+#1. It displays a welcome message. 
+#2. It asks the user how many widgets they want to order. 
+#3. It calculates the total order. 
+#4. It creates a discount for large orderss. 
+
+def ask(question)
+	print question + " "
+	gets
+end
+
+#Display the welcome message: 
+puts "welcome to the Widget Store."
+
+#Ask the user how many widgets they want to order: 
+answer = ask("How many widgets are you ordering today?")
+puts "You entered " + answer + " widgets."
+
+
+
