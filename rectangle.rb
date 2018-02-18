01b9419bb089fa06e30941b7ed73fbf8dd7dfdba
C-Lion <esstepansky@gmail.com>
2018-02-18 14:22:40 +0200

    add circle calculations and string interpolation to create neater output

--- rectangle.rb
+++ rectangle.rb
@@ -11,16 +11,41 @@
 
 
 def area(length, width)
+	print "The area of a rectangle whose length is #{length} and whose side is #{width} is "
 	length * width
 end
 
 def perimeter(length, width)
+	print "The perimeter of a rectangle whose length is #{length} and whose side is #{width} is "
 	2 * (length + width)
 end
+#These two output the sentence with a period at the end. 
+puts "#{area 2, 4}."
+puts "#{perimeter 2, 4}."
+#These two do not have a period at the end of the sentence. 
+puts area 4, 5
+puts perimeter 4, 5
 
-puts area(2, 4)
-puts perimeter(2, 4)
+#volume is a method that accepts the length, width, and height of a box
+# and returns its volume (length * width * height).
+def volume(length, width, height)
+	print "The volume of a rectangle whose length is #{length}, it's width is #{width}, and its height is #{height} is "
+	length * width * height
+end
 
-puts area(4, 5)
-puts perimeter(4, 5)
+puts volume 2, 4, 6
+
+#area_of_a_circle is a method that accepts the radius of a circle, 
+#and returns its area (Math::PI * radius ** 2).
+def area_of_a_circle(radius)
+	print "The area of a circle whose radius is #{radius}  is "
+	Math::PI * radius ** 2
+end
+
+puts "#{area_of_a_circle(2)}."
+
+puts "Of course this is all theoretical since there are not units provided!"
+
+
+
 
