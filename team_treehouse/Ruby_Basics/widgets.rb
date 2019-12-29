b4ddcf076597538ecff1e4ab5f63623151249f5a
C-Lion <esstepansky@gmail.com>
2019-12-29 14:09:00 +0200

    called chomp method on String Object to fix formatting

--- team_treehouse/Ruby_Basics/widgets.rb
+++ team_treehouse/Ruby_Basics/widgets.rb
@@ -10,18 +10,14 @@
 # 3. It calculates the total order. 
 # 4. It creates a discount for large orders. 
 
-
-# Feature 1: Display a welcome message. 
 puts "Welcome to the widget store today!"
 
-# Feature 2: Ask the user how many widgets they want to order. 
 def ask(question)
-  print question 
-  #answer = gets
-  #puts	answer
-  gets
+  print question + " " 	#space added for display looks
+  gets.chomp 			#calls method on String object to correct formatting at the end of user input
 end
 
 answer = ask("How many widgets are you ordering today?")
-puts answer
+puts "You entered #{answer} widgets."
+p answer 
 
